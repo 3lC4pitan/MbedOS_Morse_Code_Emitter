6e7f824cb08377c1293d9e6e9320c10551a0463c
Amine NAOUR <15603323+3lC4pitan@users.noreply.github.com>
2024-03-04 14:52:00 +0100

    code optimization Morse.h
    Some code optimization

--- Morse Emitter/Morse.h
+++ Morse Emitter/Morse.h
@@ -1,36 +1,31 @@
 #ifndef MORSE_H
 #define MORSE_H
 
-
 #include "DigitalOut.h"
 #include "mbed.h"
+#include <string>
 
-//#define WAIT_TIME_MS 100 
+class Morse
+{
+public:
+    Morse();
+    Morse(DigitalOut led1, DigitalOut led2, DigitalOut led3);
 
-class Morse {
-  public:
-  //methods
-  
-  Morse();
-  Morse(DigitalOut led1, DigitalOut led2, DigitalOut led3);
-
-    void newPageSig();    //new page signal
-    void startSig(); //start signal
-    void char2Morse(char c);   //translate char to morse
-    int timeInMillisec(int ms);
+    void newPageSignal();
+    void startSignal();
+    void charToMorse(char c);
+    void setTimeInMillisec(int ms);
 
 private:
     void blinkDot(int a);
     void blinkDash(int b);
-    void timeUnit(int i);  //time unit
-  
+    void timeUnit(int i);
+
 private:
-  //attributs
-  
-  DigitalOut _led1; //LED1, LED2, LED3
-  DigitalOut _led2; //LED1, LED2, LED3
-  DigitalOut _led3; //LED1, LED2, LED3  
-  
+    DigitalOut _led1;
+    DigitalOut _led2;
+    DigitalOut _led3;
+    int _timeInMillisec;
 };
 
 #endif // MORSE_H
